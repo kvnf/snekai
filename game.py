c86cab78c061a057c064516c042e1633ded91bfe
k.p <kvnfxp@protonmail.com>
2021-12-14 15:54:55 -0300

    test_game_step pass

--- game.py
+++ game.py
@@ -3,21 +3,31 @@ from enum import (
     auto,
     Enum
 )
-from random import randint
+from random import (
+    choice,
+    randint
+)
 from typing import (
+    AsyncIterable,
     List, 
     NamedTuple,
     Optional
 )
 
-
-BLOCK_SIZE: int = 10
+BLOCK_SIZE: int = 20
+SPEED = 15
 
 BLACK   =   '0x000000'
 BLUE    =   '0x0000ff'
 GREEN   =   '0x00ff00'
 RED     =   '0xff0000'
 WHITE   =   '0xffffff'
+
+STRAIGHT =   [1, 0, 0]
+RIGHT    =   [0, 1, 0]
+LEFT     =   [0, 0, 1]
+
+AI: bool = True
 class Direction(Enum):
     DOWN    =   auto()
     LEFT    =   auto()
@@ -38,52 +48,119 @@ class Game:
         self.clock = pygame.time.Clock()
         self.restart()
         
-    def game_step(self, action):
-        self._move(action)
-        self._screen_update()    
-    
     def restart(self) -> None:
         # snake stuff
         self.direction: Direction = Direction.RIGHT
-        self.head: Coord = Coord(self.width/2, self.height/2)
-        self.snake: List[Coord] = [self.head]
+        self.head: Coord = Coord(self.width//2, self.height//2)
+        self.snake: List[Coord] = [self.head, 
+                                   Coord(self.head.x - BLOCK_SIZE, self.head.y)]
         
         # env stuff
         self.score: int = 0
         self.food: Optional[Coord] = None
-        self.frame  = 0
+        self.step  = 0
         self._spawn_food()
     
-              
+    def game_step(self, action) -> bool:
+        if not AI: 
+            self.usr_input()
+        self._move(action)
+        self._snake_update()
+        game_over = False
+        if self.is_collision():
+            game_over = True
+        if self.head == self.food:
+            self.score += 1
+            self._spawn_food()
+            
+        self._screen_update()
+        self.clock.tick(SPEED)
+        self.step += 1
+        return game_over    
     
-    
+    def is_collision(self) -> bool:
+        
+        if (self.head.x > self.width or
+            self.head.y > self.height or
+            self.head.x < 0 or
+            self.head.y < 0):
+            return True
+        return False
+        
+    def usr_input(self):
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_DOWN:
+                    self.direction = Direction.DOWN
+                if event.key == pygame.K_LEFT:
+                    self.direction = Direction.LEFT
+                if event.key == pygame.K_RIGHT:
+                    self.direction = Direction.RIGHT
+                if event.key == pygame.K_UP:
+                    self.direction = Direction.UP
+                
     # private
+    def _get_direction(self, action) -> None:
+        # right -> down -> left -> up
+        directions = [Direction.RIGHT, Direction.DOWN, Direction.LEFT, Direction.UP]
+        dir_index: int = directions.index(self.direction)
+        new_index: int = dir_index
+        
+        if action == RIGHT:
+            new_index = (dir_index + 1) % 4
+        elif action == LEFT:
+            new_index = (dir_index - 1) % 4
+
+        self.direction = directions[new_index]
     
     def _move(self, action) -> None:
-        pass
-    
+
+        if AI:
+            self._get_direction(action) 
+            
+        x: int = self.head.x
+        y: int = self.head.y
+        if self.direction == Direction.RIGHT:
+            x += BLOCK_SIZE                     
+        elif self.direction == Direction.LEFT:
+            x -= BLOCK_SIZE                     
+        if self.direction == Direction.UP:
+            y -= BLOCK_SIZE                     
+        if self.direction == Direction.DOWN:
+            y += BLOCK_SIZE                     
+        self.head = Coord(x,y)
+        
     def _screen_update(self) -> None:
         self.display.fill(GREEN)
         
         for block in self.snake:
             pygame.draw.rect(self.display, WHITE, 
                              pygame.Rect(block.x, block.y, BLOCK_SIZE, BLOCK_SIZE))  
-        
+        assert self.food is not None
         pygame.draw.rect(self.display, RED, pygame.Rect(self.food.x, self.food.y, BLOCK_SIZE, BLOCK_SIZE))
         pygame.display.flip()
     
+    def _snake_update(self) -> None:
+        self.snake = [self.head]
+        
     def _spawn_food(self) -> None:
         x_blocks: int = (self.width - BLOCK_SIZE) // BLOCK_SIZE            
         y_blocks: int = (self.height - BLOCK_SIZE) // BLOCK_SIZE
         
         food_x: int  = randint(0, x_blocks) * BLOCK_SIZE            
         food_y: int  = randint(0, y_blocks) * BLOCK_SIZE
-        self.food = Coord(food_x, food_y)          
+        self.food = Coord(food_x, food_y)
+        
+        if self.food in self.snake:
+            self._spawn_food()         
+
 
 if __name__ == "__main__":
-    game = Game(300, 150)
+    game = Game(600, 400)
+    AI = False
     def play(game: Game):
-        while True:
-            game.game_step()
+        game_over = False
+        while not game_over:
+            game_over = game.game_step(choice([STRAIGHT, RIGHT, LEFT]))
 
     play(game)
--- tests/game_test.py
+++ tests/game_test.py
@@ -5,6 +5,7 @@ from typing import (
 from game import (
     BLOCK_SIZE,
     Coord,
+    Direction,
     Game
 )
 
@@ -18,20 +19,21 @@ class GameTest(TestCase):
     def test_start(self) -> None:
         game: Game = Game(100, 100)
         score: int = game.score
-        frame: int = game.frame
+        game_step: int = game.step
         snake_head: Coord = game.head
         snake = game.snake
         
         self.assertEqual(score, 0)
-        self.assertEqual(frame, 0)
+        self.assertEqual(game_step, 0)
         self.assertEqual(snake_head, Coord(50,50))
-        self.assertEqual(snake, [snake_head])
+
     
     def test_food_spawn(self) -> None:
         width: int = 40
         height: int = 40
         game: Game = Game(width, height)
-
+        
+        assert game.food is not None
         for i in range(100):
             self.assertIn(game.food.x, range(width))
             self.assertIsInstance(game.food.x, int)
@@ -44,12 +46,32 @@ class GameTest(TestCase):
         game: Game = Game(100, 100)
         head = game.head
         
+        # (-> -> -> )
         for i in range(3):
             game.game_step(STRAIGHT)
-        
         head_new_x = head.x + 3 * BLOCK_SIZE 
         head_new_y = head.y
         self.assertEqual(game.head, Coord(head_new_x, head_new_y))
+        self.assertEqual(game.direction, Direction.RIGHT)
+        # (-> -> -> ^ )
+        game.game_step(LEFT)
+        head_new_x = head.x  + 3 * BLOCK_SIZE
+        head_new_y = head.y - BLOCK_SIZE
+        self.assertEqual(game.head, Coord(head_new_x, head_new_y))
+        self.assertEqual(game.direction, Direction.UP)
         
+        # (-> -> -> ^ <-)
+        game.game_step(LEFT)
+        head_new_x = head.x + 2 * BLOCK_SIZE 
+        head_new_y = head.y - BLOCK_SIZE
+        self.assertEqual(game.head, Coord(head_new_x, head_new_y))
+        
+        # (-> -> -> ^ <- <- <- V)
+        game.game_step(STRAIGHT)
+        game.game_step(STRAIGHT)
+        game.game_step(LEFT)
+        self.assertEqual(game.head, head)
+        self.assertEqual(game.direction, Direction.DOWN)
+        self.assertEqual(game.step, 8)
         
          
